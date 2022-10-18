99f715a876c28da9e2ecf6f5253670dd4a5460b9
Lyle Dippo <ldippo421@gmail.com>
2022-10-18 13:31:36 -0400

    chore: bump ui (#59)
    bump ui
    
    Co-authored-by: Lyle Dippo <lyledippo@lyles-macbook-pro-2.eastrocklabs.com.beta.tailscale.net>

--- aptos/api/aux-ts/src/graphql/client/config.js
+++ aptos/api/aux-ts/src/graphql/client/config.js
@@ -1,21 +1,23 @@
 function getConfig() {
-  const host = window.location.host;
-  window.rest_graphql_endpoint = "https://aux.exchange/graphql";
-  window.ws_graphql_endpoint = "wss://aux.exchange/graphql";
-  if (host.match("devnet")) {
-    window.rest_graphql_endpoint = "https://devnet.aux.exchange/graphql";
-    window.ws_graphql_endpoint = "wss://devnet.aux.exchange/graphql";
-  }
-  if (host.match("testnet")) {
-    window.rest_graphql_endpoint = "https://testnet.aux.exchange/graphql";
-    window.ws_graphql_endpoint = "wss://testnet.aux.exchange/graphql";
-  }
-  if (host.match("localhost:4000")) {
-    window.rest_graphql_endpoint = "http://localhost:4000/graphql";
-    window.ws_graphql_endpoint = "ws://localhost:4000/graphql";
-  }
+    const host = window.location.host
+    window.rest_graphql_endpoint = 'https://aux.exchange/graphql'
+    window.ws_graphql_endpoint = 'wss://aux.exchange/graphql'
+    if(host.match('devnet')) {
+        window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
+    }
+    if(host.match('localhost')) {
+        window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
+    }
+    if(host.match('testnet')) {
+        window.rest_graphql_endpoint = 'https://testnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://testnet.aux.exchange/graphql'
+    }
+    
 }
 
-getConfig();
+getConfig()
 
-document.title = "DEX";
+document.title = "DEX"
+
