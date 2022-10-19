c374e950bc8ffbac04a682c5bc7f340759467f95
Lyle Dippo <lyledippo@lyles-macbook-pro-2.eastrocklabs.com.beta.tailscale.net>
2022-10-19 16:13:54 -0400

    bumpui

--- aptos/api/aux-ts/src/graphql/client/config.js
+++ aptos/api/aux-ts/src/graphql/client/config.js
@@ -8,8 +8,8 @@ function getConfig() {
         window.ws_graphql_endpoint = 'wss://aptos-mainnet.vybenetwork.xyz/graphql'
     }
     if(host.match('localhost')) {
-        window.rest_graphql_endpoint = 'https://aptos-mainnet.vybenetwork.xyz/graphql'
-        window.ws_graphql_endpoint = 'wss://aptos-mainnet.vybenetwork.xyz/graphql'
+        window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
     }
     if(host.match('testnet')) {
         window.rest_graphql_endpoint = 'https://aptos-mainnet.vybenetwork.xyz/graphql'
--- aptos/api/aux-ts/src/graphql/client/theme.js
+++ aptos/api/aux-ts/src/graphql/client/theme.js
@@ -1,46 +1,53 @@
 function createPalletteValues(key, baseColor) {
-    const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-    return weights.reduce((acc, cur) => {
-      return `${acc}--${key}-${cur}: ${baseColor[cur]};\n`
-    }, ``)
+  const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+  return weights.reduce((acc, cur) => {
+    return `${acc}--${key}-${cur}: ${baseColor[cur]};\n`
+  }, ``)
+}
+
+// TV Color Palette
+window.tvColorPalette = {
+  primary: colors.stone,
+  secondary: colors.blue,
+  blue: colors.blue,
+  green: colors.teal,
+  red: colors.red,
+}
+
+const primary = createPalletteValues('primary', colors.stone)
+const secondary = createPalletteValues('secondary', colors.teal)
+const accent = createPalletteValues('accent', colors.teal)
+const green = createPalletteValues('green', colors.teal)
+const red = createPalletteValues('red', colors.red)
+const orange = createPalletteValues('orange', colors.orange)
+const brand = createPalletteValues('accent', colors.sky)
+
+const font = 'Rubik'
+const linkEl = document.createElement('link')
+linkEl.setAttribute('rel', 'stylesheet')
+linkEl.setAttribute('href', `https://fonts.googleapis.com/css?family=${font}`)
+document.head.appendChild(linkEl)
+document.title = "Mojito"
+
+const tag = document.createElement('style')
+tag.setAttribute('type', 'text/css') 
+tag.innerHTML = `
+  :root {
+    ${primary}
+    ${secondary}
+    ${accent}
+    ${green}
+    ${orange}
+    ${red}
+    --brand-default: ${colors.teal[300]};
+    --brand-primary: ${colors.teal[400]};
+    --brand-secondary: ${colors.sky[200]};
+    --brand-gradient-start: ${colors.teal[900]};
+    --brand-gradient-mid: ${colors.teal[500]};
+    --brand-gradient-end: ${colors.sky[800]};
+    font-family: ${font}, Avenir, Helvetica, Arial, sans-serif;
   }
-  
-  const primary = createPalletteValues('primary', colors.stone)
-  const secondary = createPalletteValues('secondary', colors.teal)
-  const accent = createPalletteValues('accent', colors.teal)
-  const green = createPalletteValues('green', colors.teal)
-  const red = createPalletteValues('red', colors.red)
-  const orange = createPalletteValues('orange', colors.orange)
-  const brand = createPalletteValues('accent', colors.sky)
-  
-  
-  
-  const font = 'Rubik'
-  const linkEl = document.createElement('link')
-  linkEl.setAttribute('rel', 'stylesheet')
-  linkEl.setAttribute('href', `https://fonts.googleapis.com/css?family=${font}`)
-  document.head.appendChild(linkEl)
-  document.title = "Mojito"
-  
-  const tag = document.createElement('style')
-  tag.setAttribute('type', 'text/css') 
-  tag.innerHTML = `
-    :root {
-      ${primary}
-      ${secondary}
-      ${accent}
-      ${green}
-      ${orange}
-      ${red}
-      --brand-default: ${colors.teal[300]};
-      --brand-primary: ${colors.teal[400]};
-      --brand-secondary: ${colors.sky[200]};
-      --brand-gradient-start: ${colors.teal[900]};
-      --brand-gradient-mid: ${colors.teal[500]};
-      --brand-gradient-end: ${colors.sky[800]};
-      font-family: ${font}, Avenir, Helvetica, Arial, sans-serif;
-    }
-    `
-  
-  document.head.appendChild(tag)
+  `
+
+document.head.appendChild(tag)
   
