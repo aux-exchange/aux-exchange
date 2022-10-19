7adf0cd873bad18f792ffd6fa9415aa665da0196
Yuhan Fang <yuhan@eastrocklabs.com>
2022-10-19 14:58:12 +0000

    chore(graphql): fix isCoinRegistered endpoint

--- aptos/api/aux-ts/src/graphql/resolvers/account.ts
+++ aptos/api/aux-ts/src/graphql/resolvers/account.ts
@@ -19,15 +19,17 @@ import {
   Withdrawal,
   Position,
   Trade,
+  AccountIsCoinRegisteredArgs,
 } from "../types";
-import type { Types } from "aptos";
 
 export const account = {
-
-  async isCoinRegistered(parent: Account, coinType: Types.MoveStructTag): Promise<boolean> {
+  async isCoinRegistered(
+    parent: Account,
+    { coinType }: AccountIsCoinRegisteredArgs
+  ): Promise<boolean> {
     const coinStore = await auxClient.getAccountResourceOptional(
       parent.address,
-      `0x1::coin::CoinStore<${coinType}>`,
+      `0x1::coin::CoinStore<${coinType}>`
     );
     return coinStore !== undefined;
   },
