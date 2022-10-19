10f9c12651d439fd482f041ceca7b028b8bb3bc3
Leland Riordan <leland@eastrocklabs.com>
2022-10-18 23:11:56 -0400

    Vybe Brand

--- aptos/api/aux-ts/src/graphql/client/theme.js
+++ aptos/api/aux-ts/src/graphql/client/theme.js
@@ -1,35 +1,46 @@
 function createPalletteValues(key, baseColor) {
-  const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-  return weights.reduce((acc, cur) => {
-    return `${acc}--${key}-${cur}: ${baseColor[cur]};\n`
-  }, ``)
-}
-
-const primary = createPalletteValues('primary', colors.slate)
-const secondary = createPalletteValues('secondary', colors.blue)
-const accent = createPalletteValues('accent', colors.sky)
-const green = createPalletteValues('green', colors.emerald)
-const red = createPalletteValues('red', colors.red)
-const orange = createPalletteValues('orange', colors.orange)
-const brand = createPalletteValues('accent', colors.sky)
-
-const tag = document.createElement('style')
-tag.setAttribute('type', 'text/css') 
-tag.innerHTML = `
-  :root {
-    ${primary}
-    ${secondary}
-    ${accent}
-    ${green}
-    ${orange}
-    ${red}
-    --brand-default: #00aeef;
-    --brand-primary: #262262;
-    --brand-secondary: rgb(15 23 42);
-    --brand-gradient-start: #0b101d;
-    --brand-gradient-mid:#262262;
-    --brand-gradient-end: #003448;
+    const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    return weights.reduce((acc, cur) => {
+      return `${acc}--${key}-${cur}: ${baseColor[cur]};\n`
+    }, ``)
   }
-  `
-
-document.head.appendChild(tag)
+  
+  const primary = createPalletteValues('primary', colors.stone)
+  const secondary = createPalletteValues('secondary', colors.teal)
+  const accent = createPalletteValues('accent', colors.teal)
+  const green = createPalletteValues('green', colors.teal)
+  const red = createPalletteValues('red', colors.red)
+  const orange = createPalletteValues('orange', colors.orange)
+  const brand = createPalletteValues('accent', colors.sky)
+  
+  
+  
+  const font = 'Rubik'
+  const linkEl = document.createElement('link')
+  linkEl.setAttribute('rel', 'stylesheet')
+  linkEl.setAttribute('href', `https://fonts.googleapis.com/css?family=${font}`)
+  document.head.appendChild(linkEl)
+  document.title = "Mojito"
+  
+  const tag = document.createElement('style')
+  tag.setAttribute('type', 'text/css') 
+  tag.innerHTML = `
+    :root {
+      ${primary}
+      ${secondary}
+      ${accent}
+      ${green}
+      ${orange}
+      ${red}
+      --brand-default: ${colors.teal[300]};
+      --brand-primary: ${colors.teal[400]};
+      --brand-secondary: ${colors.sky[200]};
+      --brand-gradient-start: ${colors.teal[900]};
+      --brand-gradient-mid: ${colors.teal[500]};
+      --brand-gradient-end: ${colors.sky[800]};
+      font-family: ${font}, Avenir, Helvetica, Arial, sans-serif;
+    }
+    `
+  
+  document.head.appendChild(tag)
+  
