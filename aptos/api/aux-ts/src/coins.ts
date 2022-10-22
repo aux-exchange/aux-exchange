4e12ced3e37d2b5f85d8e33e20a2e222191b5232
Yuhan Fang <yuhan@eastrocklabs.com>
2022-10-22 19:58:59 +0000

    feat(graphql): sort pools by liquidity, featured
    Add featured status to pools. Sort by hot status, recognized USD
    stablecoin liquidity, and atomic units of liquidity.

---
+++ aptos/api/aux-ts/src/coins.ts
@@ -0,0 +1,17 @@
+export const MOJO =
+  "0x881ac202b1f1e6ad4efcff7a1d0579411533f2502417a19211cfc49751ddb5f4::coin::MOJO";
+export const APT = "0x1::aptos_coin::AptosCoin";
+export const USDC_eth =
+  "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T";
+export const USDC_sol =
+  "0xc91d826e29a3183eb3b6f6aa3a722089fdffb8e9642b94c5fcd4c48d035c0080::coin::T";
+export const SOL =
+  "0xdd89c0e695df0692205912fb69fc290418bed0dbe6e4573d744a6d5e6bab6c13::coin::T";
+export const WETH =
+  "0xcc8a89c8dce9693d354449f1f73e60e14e347417854f029db5bc8e7454008abb::coin::T";
+export const WBTC =
+  "0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T";
+export const USDA =
+  "0x1000000fa32d122c18a6a31c009ce5e71674f22d06a581bb0a15575e6addadcc::usda::USDA";
+
+export const ALL_USD_STABLES = [USDC_eth, USDC_sol, USDA];
