ae892db14f0313c74f1f2e748bc4a3663e55cfd9
Yuhan Fang <yuhan@eastrocklabs.com>
2022-10-26 14:44:57 +0000

    fix(graphql): choose mainnet-beta on mainnet-beta

--- aptos/api/aux-ts/src/graphql/client/config.js
+++ aptos/api/aux-ts/src/graphql/client/config.js
@@ -11,6 +11,10 @@ function getConfig() {
     const host = window.location.host
     window.rest_graphql_endpoint = 'https://mainnet.aux.exchange/graphql'
     window.ws_graphql_endpoint = 'wss://mainnet.aux.exchange/graphql'
+    if(host.match('mainnet-beta')) {
+        window.rest_graphql_endpoint = 'https://mainnet-beta.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://mainnet-beta.aux.exchange/graphql'
+    }
     if(host.match('devnet')) {
         window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
         window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
