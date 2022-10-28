05e0cdddc2bea5cbee3ff6cb153b92393a7ba9e2
Charles Li <charles.dt.li@gmail.com>
2022-10-27 21:14:07 -0400

    chore(ui): update

--- aptos/api/aux-ts/src/graphql/client/config.js
+++ aptos/api/aux-ts/src/graphql/client/config.js
@@ -1,29 +1,33 @@
 const links = [
     { to: '/', title: "Swap" },
     { to: '/pools', title: "Pools" },
-    { to: '/trade', title: "Trade (Coming Soon)", disabled: true },
-    { to: '/portfolio', title: "Portfolio (Coming Soon)", disabled: true }
+    { to: '/trade', title: "Trade" },
+    { to: '/portfolio', title: "Portfolio" }
   ]
 window.topnav_links = links 
+window.isAux = true
+
 function getConfig() {
     const host = window.location.host
-    window.rest_graphql_endpoint = 'https://dex.mojito.markets/graphql'
-    window.ws_graphql_endpoint = 'wss://dex.mojito.markets/graphql'
+    window.rest_graphql_endpoint = 'https://mainnet.aux.exchange/graphql'
+    window.ws_graphql_endpoint = 'wss://mainnet.aux.exchange/graphql'
     if(host.match('devnet')) {
-        window.rest_graphql_endpoint = 'https://dex.mojito.markets/graphql'
-        window.ws_graphql_endpoint = 'wss://dex.mojito.markets/graphql'
+        window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
     }
     if(host.match('localhost')) {
+        // window.rest_graphql_endpoint = 'http://localhost:4000/graphql'
+        // window.ws_graphql_endpoint = 'ws://localhost:4000/graphql'
         window.rest_graphql_endpoint = 'https://devnet.aux.exchange/graphql'
         window.ws_graphql_endpoint = 'wss://devnet.aux.exchange/graphql'
     }
     if(host.match('testnet')) {
-        window.rest_graphql_endpoint = 'https://dex.mojito.markets/graphql'
-        window.ws_graphql_endpoint = 'wss://dex.mojito.markets/graphql'
+        window.rest_graphql_endpoint = 'https://testnet.aux.exchange/graphql'
+        window.ws_graphql_endpoint = 'wss://testnet.aux.exchange/graphql'
     }
     
 }
 
 getConfig()
 
-document.title = "Mojito"
+document.title = "AUX DEX"
