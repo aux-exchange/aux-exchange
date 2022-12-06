aca3b9d02a84fbec23fbbed958239730870ba630
Charles Li <charles.dt.li@gmail.com>
2022-12-06 19:17:36 +0000

    fix(graphql): remove broken pyth conn

--- aptos/api/aux-ts/src/graphql/client.ts
+++ aptos/api/aux-ts/src/graphql/client.ts
@@ -1,9 +1,3 @@
-import {
-  getPythProgramKeyForCluster,
-  PythConnection,
-  PythHttpClient,
-} from "@pythnetwork/client";
-import { Connection } from "@solana/web3.js";
 import { RedisPubSub } from "graphql-redis-subscriptions";
 import * as redis from "redis";
 import { AuxClient } from "../client";
@@ -16,7 +10,7 @@ export const redisClient = redis.createClient();
 redisClient.on("error", (err) => console.error("[Redis]", err));
 export const redisPubSub = new RedisPubSub();
 
-const connection = new Connection("https://solana-api.projectserum.com");
-const pythPublicKey = getPythProgramKeyForCluster("mainnet-beta");
-export const pythClient = new PythHttpClient(connection, pythPublicKey);
-export const pythConnection = new PythConnection(connection, pythPublicKey);
+// const connection = new Connection("https://solana-api.projectserum.com");
+// const pythPublicKey = getPythProgramKeyForCluster("mainnet-beta");
+// export const pythClient = new PythHttpClient(connection, pythPublicKey);
+// export const pythConnection = new PythConnection(connection, pythPublicKey);
