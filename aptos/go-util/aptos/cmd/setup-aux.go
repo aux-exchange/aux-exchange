3f68061824ce950357d8bb0d22d784502942668e
Charles Li <charles.dt.li@gmail.com>
2022-12-09 00:36:53 +0000

    chore: move aptos tooling under aptos/ and update readmes

--- aptos/go-util/aptos/cmd/setup-aux.go
+++ aptos/go-util/aptos/cmd/setup-aux.go
@@ -25,7 +25,7 @@ If the profile for the chosen network exists, use that. Othwerise a new key will
 
 	network := aptos.Localnet
 	genNewKey := false
-	workDir := "aptos/contract"
+	workDir := "contract"
 	skipLocalNet := false
 	seed := "aux"
 	overrideUrl := ""
