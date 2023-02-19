03c14ae34777f438ecf536ee312df15fce8e1bc7
Chao Xu <fardream@users.noreply.github.com>
2023-02-18 23:39:09 -0500

    add bootstrap

--- aptos/simple-ui/pages/_app.tsx
+++ aptos/simple-ui/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  });
+  return <Component {...pageProps} />;
 }
