dbf933791a6f79ea6140cb8a291438c3bc4bdde1
Chao Xu <fardream@users.noreply.github.com>
2023-03-06 18:46:55 -0500

    rename aux folder to auxech for Microsoft Windows.
    See https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file

--- go-util/generate.go
+++ go-util/generate.go
@@ -4,13 +4,13 @@
 package aux_go_generate
 
 // critbit
-//go:generate go run github.com/fardream/gen-move-container@latest critbit -o ../aptos/contract/aux/sources/critbit_v.move -p aux -m critbit_v
-//go:generate go run github.com/fardream/gen-move-container@latest critbit -o ../aptos/contract/aux/sources/critbit.move -p aux -m critbit --use-aptos-table
+//go:generate go run github.com/fardream/gen-move-container@latest critbit -o ../aptos/contract/auxexch/sources/critbit_v.move -p aux -m critbit_v
+//go:generate go run github.com/fardream/gen-move-container@latest critbit -o ../aptos/contract/auxexch/sources/critbit.move -p aux -m critbit --use-aptos-table
 
 // reward distributor
-//go:generate go run ./aptos/cmd/gen-reward-distributor -o ../aptos/contract/aux/sources/reward_distributor.move
-//go:generate go run ./aptos/cmd/gen-reward-distributor -q -o ../aptos/contract/aux/sources/reward_quoter.move
+//go:generate go run ./aptos/cmd/gen-reward-distributor -o ../aptos/contract/auxexch/sources/reward_distributor.move
+//go:generate go run ./aptos/cmd/gen-reward-distributor -q -o ../aptos/contract/auxexch/sources/reward_quoter.move
 
 // abort-only-contract
 // Uncomment the below command to generate abort only contract
-// //go:generate go run ./cmd/move-abort -e authority.move -i ../aptos/contract/aux/sources -o ../aptos/abort-only-contract/aux/sources
+// //go:generate go run ./cmd/move-abort -e authority.move -i ../aptos/contract/auxexch/sources -o ../aptos/abort-only-contract/auxexch/sources
